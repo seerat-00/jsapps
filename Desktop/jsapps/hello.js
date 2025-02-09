2caf47f7ea9b06eca1a7ad62e788556e25645fee
Seerat Sharma <isharmaseerat13@gmail.com>
2025-02-09 23:36:15 +0530

    second commit

--- Desktop/jsapps/hello.js
+++ Desktop/jsapps/hello.js
@@ -13,7 +13,7 @@ function checkEvenOdd(number) {
     if (number % 2 === 0) {
         console.log(number + " is even.")
     } else {
-        console.log(number + " is odd.")
+        console.log(number + " is odd.....")
     }
 }
 
