27b8eb32265763c01856724631040619851397d7
Seerat Sharma <isharmaseerat13@gmail.com>
2025-02-09 23:21:22 +0530

    first commit

---
+++ Desktop/jsapps/hello.js
@@ -0,0 +1,20 @@
+// This is a simple JavaScript program
+
+// Declare a variable
+let greeting = "Hello, World!"
+
+// Function to display the greeting
+function displayGreeting() {
+    console.log(greeting)
+}
+
+// Function to check if a number is even or odd
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        console.log(number + " is even.")
+    } else {
+        console.log(number + " is odd.")
+    }
+}
+
+// Call the functions
